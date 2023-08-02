782ef123ca2020cd1ba6dc9cbc06077dbb3a9792
ikhsanadrians <aikhsan032@gmail.com>
2023-08-02 14:54:33 +0700

    mass update

---
+++ src/components/NewPost.tsx
@@ -0,0 +1,36 @@
+import ProfilePicture from '../../public/assets/images/rpg.jpg';
+
+export default function NewPost() {
+  return (<div className="new-posts bg-white w-full px-4 py-2 rounded-md border-[1.5px] border-gray-300">
+    <div className="flex flex-col">
+      <div className="profile-n-text w-full flex gap-3 mt-1">
+        <div className="post-profile-pic h-8 w-8 border-[1.7px] border-gray-300 rounded-full overflow-hidden object-cover">
+          <img src={ProfilePicture} alt="profile-picture" className='object-cover h-full w-full' />
+        </div>
+        <input className="w-full bg-gray-100 border-[1.5px] border-gray-300 px-3 rounded-2xl" type="text" placeholder='Apa yang ingin Anda tanyakan dan bagikan?' />
+      </div>
+      <div className="new-posts-options w-full flex items-center mt-3 justify-around">
+        <div className="ask basis-[33%] cursor-pointer justify-center hover:bg-gray-100 hover:rounded-lg flex items-center gap-1 border-r-[1.2px] mr-1 border-gray-300 px-4">
+          <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><g className="icon_svg-stroke" stroke="#666" stroke-width="1.5" fill="none" fill-rule="evenodd"><g transform="translate(9 7)"><path d="M3 6v-.5A2.5 2.5 0 1 0 .5 3" stroke-linecap="round" stroke-linejoin="round"></path><circle className="icon_svg-fill_as_stroke" fill="#666" cx="3" cy="8.5" r="1" stroke="none"></circle></g><path d="M7.5 4h9a3 3 0 0 1 3 3v9a3 3 0 0 1-3 3h-3L9 22v-3H7.5a3 3 0 0 1-3-3V7a3 3 0 0 1 3-3Z" stroke-linejoin="round"></path></g></svg>
+          <p className='font-semibold text-gray-500 text-sm'>
+            Tanya
+          </p>
+        </div>
+        <div className="answers basis-[33%] cursor-pointer justify-center flex items-center gap-1 hover:bg-gray-100  hover:rounded-lg  border-r-[1.2px] border-gray-300 px-4">
+          <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><g stroke-width="1.5" fill="none" fill-rule="evenodd"><path d="M18.571 5.429h0a2 2 0 0 1 0 2.828l-9.9 9.9-4.24 1.416 1.412-4.245 9.9-9.9h0a2 2 0 0 1 2.828 0Z" className="icon_svg-stroke" stroke="#666" stroke-linecap="round" stroke-linejoin="round"></path><path className="icon_svg-fill_as_stroke" fill="#666" d="m4.429 19.571 2.652-.884-1.768-1.768z"></path><path d="M14.5 19.5h5v-5m-10-10h-5v5" className="icon_svg-stroke" stroke="#666" stroke-linecap="round" stroke-linejoin="round"></path></g></svg>
+          <p className='font-semibold text-gray-500 text-sm'>
+            Jawab
+          </p>
+        </div>
+        <div className="posts basis-[33%] cursor-pointer justify-center flex items-center gap-1 hover:bg-gray-100  hover:rounded-lg py-1 px-4">
+          <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><path d="M18.571 5.429h0a2 2 0 0 1 0 2.828l-9.9 9.9-4.24 1.416 1.412-4.245 9.9-9.9a2 2 0 0 1 2.828 0Z" className="icon_svg-stroke" stroke="#666" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path><path className="icon_svg-fill_as_stroke" fill="#666" d="m4.429 19.571 2.652-.884-1.768-1.768z"></path></g></svg>
+          <p className='font-semibold text-gray-500 text-sm'>
+            Kiriman
+          </p>
+        </div>
+
+      </div>
+    </div>
+  </div>
+  )
+}
---
+++ src/components/SidebarRoom.tsx
@@ -0,0 +1,107 @@
+import cnximg from '../../public/assets/images/cnx.png';
+import js from '../../public/assets/images/js.png';
+import fisika from '../../public/assets/images/fisika.png';
+import biologi from '../../public/assets/images/biologi.jpg';
+
+export default function SidebarRoom() {
+    return (
+      <div className="sidebar-room">
+        <ul className='flex flex-col gap-4'>
+            <li className="cursor-pointer">
+                <div className="room flex gap-2 text-base items-center bg-gray-200 max-w-fit py-[4px] px-2 rounded-md">
+                    <div className="icons bg-gray-100 p-[1px] rounded-sm">
+                        <svg width="18" height="18" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M4.5 12h15M12 4.5v15" className="icon_svg-stroke" stroke="#666" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round"></path></svg>
+                    </div>
+                    <p className='text-gray-500 text-sm'>
+                        Buat Ruang
+                    </p>
+                </div>
+            </li>
+            <li className="cursor-pointer">
+                <div className="room flex gap-2 text-base items-center  max-w-[7.2rem] py-[4px] px-2 rounded-md max-h-fit truncate hover:bg-gray-200">
+                    <div className="icons-rooms bg-gray-100 p-[1px] w-[1.3rem] truncate h-[1.3rem] overflow-hidden object-cover rounded-sm">
+                        <img src={js} className='object-cover' />
+                    </div>
+                    <p className='text-gray-500 text-sm w-[4.4rem] truncate'>
+                        Web Development design
+                    </p>
+                </div>
+            </li>
+            <li className="cursor-pointer">
+                <div className="room flex gap-2 text-base items-center  max-w-[7.2rem] py-[4px] px-2 rounded-md max-h-fit truncate hover:bg-gray-200">
+                    <div className="icons-rooms bg-gray-100 p-[1px] w-[1.3rem] truncate h-[1.3rem] overflow-hidden object-cover rounded-sm">
+                        <img src={biologi} className='object-cover' />
+                    </div>
+                    <p className='text-gray-500 text-sm w-[4.4rem] truncate'>
+                        Kimia
+                    </p>
+                </div>
+            </li>
+            <li className="cursor-pointer">
+                <div className="room flex gap-2 text-base items-center  max-w-[7.2rem] py-[4px] px-2 rounded-md max-h-fit truncate hover:bg-gray-200">
+                    <div className="icons-rooms bg-gray-100 p-[1px] w-[1.3rem] truncate h-[1.3rem] overflow-hidden object-cover rounded-sm">
+                        <img src={fisika} className='object-cover' />
+                    </div>
+                    <p className='text-gray-500 text-sm w-[4.4rem] truncate'>
+                        Fisika
+                    </p>
+                </div>
+            </li>
+            <li className="cursor-pointer">
+                <div className="room flex gap-2 text-base items-center ax-w-[7.2rem] py-[4px] px-2 rounded-md max-h-fit max-w-fit hover:bg-gray-200">
+                    <div className="icons-rooms bg-gray-100 p-[1px] w-[1.3rem] h-[1.3rem] overflow-hidden object-cover rounded-sm">
+                        <img src={cnximg} className='object-cover' />
+                    </div>
+                    <p className='text-gray-500 text-sm truncate w-[4.4rem]'>
+                        Connexsoft
+                    </p>
+                </div>
+            </li>
+            <li className="cursor-pointer">
+                <div className="room flex gap-2 text-base items-center ax-w-[7.2rem] py-[4px] px-2 rounded-md max-h-fit max-w-fit hover:bg-gray-200">
+                    <div className="icons-rooms bg-gray-100 p-[1px] w-[1.3rem] h-[1.3rem] overflow-hidden object-cover rounded-sm">
+                        <img src={cnximg} className='object-cover' />
+                    </div>
+                    <p className='text-gray-500 text-sm truncate w-[4.4rem]'>
+                        Connexsoft
+                    </p>
+                </div>
+            </li>
+            <li className="cursor-pointer">
+                <div className="room flex gap-2 text-base items-center ax-w-[7.2rem] py-[4px] px-2 rounded-md max-h-fit max-w-fit hover:bg-gray-200">
+                    <div className="icons-rooms bg-gray-100 p-[1px] w-[1.3rem] h-[1.3rem] overflow-hidden object-cover rounded-sm">
+                        <img src={cnximg} className='object-cover' />
+                    </div>
+                    <p className='text-gray-500 text-sm truncate w-[4.4rem]'>
+                        Connexsoft
+                    </p>
+                </div>
+            </li>
+            <li className="cursor-pointer">
+                <div className="room flex gap-2 text-base items-center ax-w-[7.2rem] py-[4px] px-2 rounded-md max-h-fit max-w-fit hover:bg-gray-200">
+                    <div className="icons-rooms bg-gray-100 p-[1px] w-[1.3rem] h-[1.3rem] overflow-hidden object-cover rounded-sm">
+                        <img src={cnximg} className='object-cover' />
+                    </div>
+                    <p className='text-gray-500 text-sm truncate w-[4.4rem]'>
+                        Connexsoft
+                    </p>
+                </div>
+            </li>
+
+            <li className="cursor-pointer">
+                <div className="room flex gap-2 text-base items-center ax-w-[7.2rem] py-[4px] px-2 rounded-md max-h-fit max-w-fit hover:bg-gray-200">
+                    <div className="icons-rooms bg-gray-100 p-[1px] w-[1.3rem] h-[1.3rem] overflow-hidden object-cover rounded-sm">
+                        <img src={cnximg} className='object-cover' />
+                    </div>
+                    <p className='text-gray-500 text-sm truncate w-[4.4rem]'>
+                        Connexsoft
+                    </p>
+                </div>
+            </li>
+        </ul>
+        <div className="footer">
+
+        </div>
+    </div>
+    )
+}

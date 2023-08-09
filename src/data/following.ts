116ff2b2af611bae97ae93861ec7576dc45efcda
ikhsanadrians <aikhsan032@gmail.com>
2023-08-10 05:47:30 +0700

    [UPDATE] Dummy Data for PostFollowing

---
+++ src/data/following.ts
@@ -0,0 +1,45 @@
+type PostProps = {
+    author? : string,
+    authorPPUrl? : string,
+    authorCredentials? : string,
+    question? : string,
+    answer? : string,
+    thumbnailUrl?: string, 
+    postDate? : string,
+    upvoteValue?: string,
+    commentValue?: number,
+    shareValue?: number
+}
+
+
+let PostFollowingData : PostProps[] = [
+    {
+        author: "Ardiyan Syahbani",
+        authorCredentials: "Sound Engineer Syncronize Fest Sejak 2018  ·  5 Agustus",
+        postDate: "7 Agustus",
+        authorPPUrl: "https://res.cloudinary.com/dewnyzbmg/image/upload/v1691344285/WhatsApp_Image_2022-03-27_at_09.17.06_uj3ue8.jpg",
+        question: "Pribadi terkenal mana yang diidolakan dan dikenal dalam sejarah yang sebenarnya adalah seorang yang mengerikan?",
+        answer: "Hadirin sekalian, saya persembahkan kepada Anda John Lennon:",
+        thumbnailUrl: "https://qph.cf2.quoracdn.net/main-qimg-2804660e569b8607bb36c78746f279bd-lq",
+        upvoteValue: "1.2rb",
+        commentValue: 125,
+        shareValue: 56
+    },
+    {
+        author:"Faris Maulana Al-Bai",
+        authorCredentials:"Unity Game Developer Di Agate Indonesia  ·  7 Agustus",
+        postDate: "7 Agustus",
+        authorPPUrl: "https://res.cloudinary.com/dewnyzbmg/image/upload/v1691343076/imageedit_1_9296104578_cj4e4u.png",
+        question: "Apa strategi terbaik untuk Menjadi Game Developer",
+        thumbnailUrl: "https://binus.ac.id/wp-content/uploads/2020/10/44-0-Tertarik-Jadi-Game-Developer-Profesional-Jurusan-Kuliah-Ini-Cocok-Untukmu.jpg",
+        upvoteValue: "1.2rb",
+        commentValue: 125,
+        shareValue: 56,
+        answer: "Menjadi seorang pengembang game adalah tujuan yang menarik, tetapi juga memerlukan komitmen, keterampilan, dan usaha yang serius. Berikut adalah beberapa strategi yang dapat membantu Anda dalam perjalanan untuk menjadi seorang pengembang game",
+    },
+    
+   
+]
+
+
+export default PostFollowingData
--- src/pages/Following.tsx
+++ src/pages/Following.tsx
@@ -4,6 +4,7 @@ import NewPost from '../components/NewPost';
 import ThePost from '../components/ThePost';
 import Container from '../components/layouts/Container';
 import Ads from '../components/Ads';
+import PostFollowingData from '../data/following';
 
 function Following() {
   const location = useLocation();
@@ -14,12 +15,9 @@ function Following() {
         <SidebarRoom />
         <div className="posts w-full lg:w-[60%]">
           <NewPost />
-          <ThePost author='Maulana Hafizh Sirajudin' authorCredentials='Linux Enthusiast | Software Engineering Student at SMKN 10 Jakarta  ·  7 Agustus' postDate='7 Agustus' authorPPUrl='https://regmedia.co.uk/2016/08/26/torvalds.jpg' question='Distro Linux Manakah Terbaik Untuk Laptop Low Spec?' answer='Pemilihan distribusi Linux terbaik untuk laptop dengan spesifikasi rendah dapat tergantung pada preferensi pengguna, kebutuhan, dan keahlian teknis. Beberapa distribusi Linux yang biasanya dianggap cocok untuk laptop dengan spesifikasi rendah adalah:' thumbnailUrl='https://linuxconfig.org/wp-content/uploads/2020/05/01-arch-linux-download.png' upvoteValue={'967'} commentValue={95} shareValue={68} />
-          <ThePost author='Faris Maulana Al-Bai' authorCredentials='Unity Game Developer Di Agate Indonesia  ·  7 Agustus' postDate='7 Agustus' authorPPUrl='https://res.cloudinary.com/dewnyzbmg/image/upload/v1691343076/imageedit_1_9296104578_cj4e4u.png' question='Apa strategi terbaik untuk mengoptimalkan kinerja dan pengalaman pengguna dalam pengembangan game yang kompleks dan membutuhkan sumber daya tinggi?' answer='Untuk mengoptimalkan kinerja dan pengalaman pengguna dalam pengembangan game yang kompleks dan membutuhkan sumber daya tinggi, ada beberapa strategi yang dapat diimplementasikan:
-          Optimasi Kode: Tinjau dan perbaiki kode sumber game secara teratur untuk ' thumbnailUrl='https://binus.ac.id/wp-content/uploads/2020/10/44-0-Tertarik-Jadi-Game-Developer-Profesional-Jurusan-Kuliah-Ini-Cocok-Untukmu.jpg' upvoteValue={'1.2rb'} commentValue={125} shareValue={56} />
-            <ThePost author='Ardiyan Syahbani' authorCredentials='Sound Engineer Syncronize Fest Sejak 2018  ·  5 Agustus' postDate='7 Agustus' authorPPUrl='https://res.cloudinary.com/dewnyzbmg/image/upload/v1691344285/WhatsApp_Image_2022-03-27_at_09.17.06_uj3ue8.jpg' question='Pribadi terkenal mana yang diidolakan dan dikenal dalam sejarah yang sebenarnya adalah seorang yang mengerikan?' answer='Hadirin sekalian, saya persembahkan kepada Anda John Lennon:' thumbnailUrl='https://qph.cf2.quoracdn.net/main-qimg-2804660e569b8607bb36c78746f279bd-lq' upvoteValue={'1.2rb'} commentValue={125} shareValue={56} />
-            <ThePost author='Faris Maulana Al-Bai' authorCredentials='Unity Game Developer Di Agate Indonesia  ·  7 Agustus' postDate='7 Agustus' authorPPUrl='https://res.cloudinary.com/dewnyzbmg/image/upload/v1691343076/imageedit_1_9296104578_cj4e4u.png' question='Apa strategi terbaik untuk mengoptimalkan kinerja dan pengalaman pengguna dalam pengembangan game yang kompleks dan membutuhkan sumber daya tinggi?' answer='Untuk mengoptimalkan kinerja dan pengalaman pengguna dalam pengembangan game yang kompleks dan membutuhkan sumber daya tinggi, ada beberapa strategi yang dapat diimplementasikan:
-          Optimasi Kode: Tinjau dan perbaiki kode sumber game secara teratur untuk ' thumbnailUrl='https://binus.ac.id/wp-content/uploads/2020/10/44-0-Tertarik-Jadi-Game-Developer-Profesional-Jurusan-Kuliah-Ini-Cocok-Untukmu.jpg' upvoteValue={'1.2rb'} commentValue={125} shareValue={56} />
+          {
+                      PostFollowingData.map((index,key)=> <ThePost key={key} answer={index.answer} author={index.author} authorCredentials={index.authorCredentials} authorPPUrl={index.authorPPUrl} commentValue={index.commentValue} postDate={index.postDate} question={index.question} shareValue={index.shareValue} thumbnailUrl={index.thumbnailUrl} upvoteValue={index.upvoteValue} /> )
+          }
         </div>
         <Ads/>
       </Container>
